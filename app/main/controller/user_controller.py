43963138e737bb8dfd96c8b296bd81eb74ed498c
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2020-04-12 16:38:59 +0900

    Add Self token authentication. it will diallow when try others data modifiy

--- app/main/controller/user_controller.py
+++ app/main/controller/user_controller.py
@@ -1,13 +1,19 @@
 from flask import request
 from flask_restx import Resource
 
+from ..util.decorator import self_token_required
 from ..util.dto import UserDto
 from ..service.user_service import save_new_user, get_all_users, get_a_user, update_a_user
+
 
 api = UserDto.api
 _signup = UserDto.signup
 _lookup = UserDto.lookup
 _profile = UserDto.profile
+
+auth_parser = api.parser()
+auth_parser.add_argument('Authorization', location='headers',
+                         help='authentication token. should pass "Bearer {{ token }}"', required=True)
 
 
 @api.route('/')
@@ -45,11 +51,16 @@ class User(Resource):
         return None
 
     @api.doc('update user profile')
-    @api.expect(_profile, validate=True)
+    # @api.expect(_profile, validate=True)
+    @api.doc(body=_profile, parser=auth_parser)
+    @self_token_required
     def patch(self, id):
+        """patch a user given it identifier"""
         data = request.json
         user = get_a_user(id)
         if not user:
             api.abort(404)
+        elif 'admin' in data and not user.admin:
+            api.abort(401)
 
         return update_a_user(id, data)
