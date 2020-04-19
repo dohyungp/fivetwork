602dcc466f44015e7e0a5f4884768fb03fc7e6bc
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2020-04-20 00:07:13 +0900

    Add load_dontenv

--- fivetwork/main/__init__.py
+++ fivetwork/main/__init__.py
@@ -1,7 +1,8 @@
+from pathlib import Path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
+from dotenv import load_dotenv
 from .config import config_by_name
 
 db = SQLAlchemy()
@@ -10,6 +11,10 @@ flask_bcrypt = Bcrypt()
 
 def create_app(config_name):
     app = Flask(__name__)
+    if config_name == 'prod':
+        root_dir = Path(__file__).absolute().parents[2]
+        env_file = root_dir.joinpath('.flaskenv')
+        load_dotenv(env_file)
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
     flask_bcrypt.init_app(app)
--- fivetwork/test/base.py
+++ fivetwork/test/base.py
@@ -1,4 +1,6 @@
+from pathlib import Path
 from flask_testing import TestCase
+from dotenv import load_dotenv
 from fivetwork.main import db
 from manage import app
 
@@ -8,6 +10,9 @@ class BaseTestCase(TestCase):
 
     def create_app(self):
         app.config.from_object('fivetwork.main.config.TestingConfig')
+        root_dir = Path(__file__).absolute().parents[2]
+        env_file = root_dir.joinpath('.flaskenv')
+        load_dotenv(env_file)
         return app
 
     def setUp(self):
